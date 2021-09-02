e62127e853721ef5aefd67791b732db571560db5
4ov <mma7200@aol.com>
2021-09-02 17:25:32 +0200

    1 file modified

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
 // run `node index.js` in the terminal
 
-console.log(`Hello Node.js v${process.versions.node}!`);
+const createServer = require('http').createServer;
+
+// console.log(`Hello Node.js v${process.versions.node}!`);
+
+console.log(
+  createServer((req, res) => {
+    res.end('o');
+  }).listen(3000)
+);
